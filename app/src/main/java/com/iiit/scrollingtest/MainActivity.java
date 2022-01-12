17a8cc01de95a28a990b988eec3bc10defe37563
SauravMahalik <80640726+SauravMahalik@users.noreply.github.com>
2022-01-12 21:20:42 +0530

    Scrolling Text

---
+++ app/src/main/java/com/iiit/scrollingtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iiit.scrollingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
